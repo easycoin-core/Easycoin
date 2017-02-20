7d8c17772a1ce20ce34a2e84fb46534ef871b6ea
easycoin-core <easycoin6262@gmail.com>
2017-02-20 13:44:46 +0100

    Add files via upload

--- qa/pull-tester/rpc-tests.py
+++ qa/pull-tester/rpc-tests.py
@@ -75,10 +75,10 @@ for arg in sys.argv[1:]:
         opts.add(arg)
 
 #Set env vars
-if "EASYCOIND" not in os.environ:
-    os.environ["EASYCOIND"] = BUILDDIR + '/src/easycoind' + EXEEXT
-if "EASYCOINCLI" not in os.environ:
-    os.environ["EASYCOINCLI"] = BUILDDIR + '/src/easycoin-cli' + EXEEXT
+if "LITECOIND" not in os.environ:
+    os.environ["LITECOIND"] = BUILDDIR + '/src/litecoind' + EXEEXT
+if "LITECOINCLI" not in os.environ:
+    os.environ["LITECOINCLI"] = BUILDDIR + '/src/litecoin-cli' + EXEEXT
 
 if EXEEXT == ".exe" and "-win" not in opts:
     # https://github.com/bitcoin/bitcoin/commit/d52802551752140cf41f0d9a225a43e84404d3e9
